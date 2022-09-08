2bc29480a46188e58a2450f1d1f4b46dc6d65334
unknown <hassankhan5805@gmail.com>
2022-09-09 00:58:23 +0500

    ios update

---
+++ lib/data/utils.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+extension QueryParameterAdd on Map {
+  addParam(key, value) {
+    if (value != null) {
+      this[key] = value;
+    }
+  }
+}
+
+List<dynamic> record = [];
+List<Map<String, int>> recordMap = [];
+
+var dio = Dio();
+
+///Timer
+late Timer timer;
+
+///SharedPreferences
+late SharedPreferences? preferences;
+
+///Platforms
+AndroidDeviceInfo? androidInfo;
+IosDeviceInfo? iosDeviceInfo;
+
+///App Settings
+String? appLogoUrl;
+Directory? appDocDir;
+
+///URL
+const BASE_URL = "https://www.autotherapia.com/";
+const String apiEndpoint = BASE_URL + "/wp-json/ms_lms/v2/";
